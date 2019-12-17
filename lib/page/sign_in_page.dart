f08f842884c5f478ff1f3d82281a2c0ad52bad00
Alexandre Lima <alexlima.crvg@gmail.com>
2019-12-16 23:42:52 -0300

    Formatando com gradient azul no centro

--- lib/page/sign_in_page.dart
+++ lib/page/sign_in_page.dart
@@ -9,31 +9,88 @@ class SignInPage extends StatefulWidget {
 }
 
 class _SignInPageState extends State<SignInPage> {
-
   @override
   void initState() {
     super.initState();
     print(_auth);
-    _auth.signInWithEmailAndPassword(email: 'rodmaster@gmail.com', password: '12345678')
-      .then((authResult) => print(authResult.user.email));
+    _auth
+        .signInWithEmailAndPassword(
+            email: 'rodmaster@gmail.com', password: '12345678')
+        .then((authResult) => print(authResult.user.email));
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
+      body: Center(
+        child: Material(
+          elevation: 2,
+          clipBehavior: Clip.antiAlias,
+          borderRadius: BorderRadius.circular(16),
           child: Form(
-            child: Padding(
-              padding: const EdgeInsets.all(38),
+            child: Container(
+              padding: const EdgeInsets.all(24),
+              constraints: BoxConstraints(maxWidth: 400),
+              height: 400,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: <Color>[
+                    Colors.lightBlue[900],
+                    Colors.lightBlue[700],
+                    Colors.lightBlue,
+                  ],
+                ),
+              ),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
                 children: <Widget>[
-                  TextFormField(),
-                  TextFormField(),
+                  Text(
+                    'FAÇA LOGIN',
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Usuário',
+                      labelStyle: TextStyle(color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.black12,
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      labelText: 'Senha',
+                      labelStyle: TextStyle(color: Colors.white),
+                      filled: true,
+                      fillColor: Colors.black12,
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                        borderSide: BorderSide.none,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
                   RaisedButton(
-                    onPressed: null,
-                    child: Text('Login'),
-                  )
+                    padding: const EdgeInsets.symmetric(
+                      vertical: 4,
+                      horizontal: 24,
+                    ),
+                    color: Colors.cyan[300],
+                    textColor: Colors.white,
+                    child: Text('ENTRAR'),
+                    onPressed: () {},
+                  ),
                 ],
               ),
             ),
